bd0cc880ee150c389e1934aaef45dc8935b4f671
invincible003 <upendrajput3964@gmail.com>
2024-03-12 12:59:05 +0530

    name changed wallpaper

---
+++ name.py
@@ -0,0 +1,14 @@
+import os
+
+directory_path = "Wallpaper"
+
+files = os.listdir(directory_path)
+
+start_count = 0
+
+for file in files:
+    file_name = os.path.join(directory_path, file)
+    new_name = f"nature {start_count}"
+    new_file_path = os.path.join(directory_path, new_name)
+    os.rename(file_name, new_file_path)
+    start_count += 1

2040ac04d2be134055ee695dfb897fad7bde6813
invincible003 <upendrajput3964@gmail.com>
2024-03-22 09:50:07 +0530

    extensions.

--- name.py
+++ name.py
@@ -8,7 +8,7 @@ start_count = 0
 
 for file in files:
     file_name = os.path.join(directory_path, file)
-    new_name = f"nature {start_count}"
+    new_name = f"nature {start_count}.jpg"
     new_file_path = os.path.join(directory_path, new_name)
     os.rename(file_name, new_file_path)
     start_count += 1
